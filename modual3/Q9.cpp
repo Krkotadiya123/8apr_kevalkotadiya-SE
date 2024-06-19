e5d23a05151e0c92bb510faa087d667e4c6b4a76
Krkotadiya123 <167281609+Krkotadiya123@users.noreply.github.com>
2024-06-19 11:28:17 +0530

    Delete modual3 directory

--- modual3/Q9.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-main()
-{
-	//WAP to find number is even or odd using ternary operator
-	int num;
-	printf("Enter Value :");
-	scanf("%d",&num);
-	(num%2==0) ? printf("%d is even number",num) : printf("%d is odd number ",num);
-}
--- modual3/q1.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-main(){
-	//Display Information using printf
-	printf("Name : Rahul Kanara");
-	printf("\nBirth Date : 1/6/2004 ");
-	printf("\nAge : 20 ");
-	printf("\nAddress : Rampara ");
-	
-}
