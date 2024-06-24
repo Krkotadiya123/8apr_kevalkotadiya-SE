cd0cf8553b9825bf1a41e1b8f787ebf87e674eb2
keval <167281609+Krkotadiya123@users.noreply.github.com>
2024-06-24 10:31:29 +0530

    Delete Assignment/modual3 directory

--- Assignment/modual3/Q11.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-main(){
-	//WAP to print 972 to 897 using for loop
-	int a=972;
-	for(a;a>=897;a--)
-	{
-		printf("%d\n",a);
-	}
-}
--- Assignment/modual3/modual 4/Q_1.cpp
+++
@@ -1,6 +0,0 @@
-//WAP to print “Hello World” using C++
-#include<iostream>
-using namespace std;
-main(){
-	cout<<"hello world";
-}
